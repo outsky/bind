cd1bea823e0090eac51b6cb9a8f7754b2bb9aa06
outsky <317525370@qq.com>
2014-01-18 16:45:14 +0800

    use template to judge the concrete class type (const/pointer/ref)

--- bind.h
+++ bind.h
@@ -7,7 +7,7 @@
 
 // non-member function with 2 arguments
 template <typename R, typename B1, typename B2, typename A1, typename A2>
-bind_t<R,normal_fn<R, R (*)(B1,B2)>,arg_list2<A1,A2> >
+bind_t<R, normal_fn<R, R (*)(B1,B2)>, arg_list2<A1,A2> >
 bind(R (*f)(B1,B2), A1 a1, A2 a2) {
     typedef R (*FN)(B1, B2);
     typedef normal_fn<R, FN> F;
@@ -15,44 +15,24 @@ bind(R (*f)(B1,B2), A1 a1, A2 a2) {
     return bind_t<R,F,arg_list2<A1,A2> >(F(f), l2);
 }
 
-// member function with 2 arguments, pass class object by reference
-template <typename R, typename C, typename B1, typename B2, typename A1, typename A2>
-bind_t<R,member_fn<C,R,R (C::*)(B1,B2)>,arg_list3<C&,A1,A2> >
-bind(R (C::*f)(B1,B2), C& c, A1 a1, A2 a2) {
-    typedef R (C::*FN)(B1, B2);
-    typedef member_fn<C, R, FN> F;
-    arg_list3<C&,A1,A2> l3(c, a1, a2);
-    return bind_t<R,F,arg_list3<C&,A1,A2> >(F(f), l3);
+// member function with 2 arguments
+template <typename R, typename T, typename C, typename B1, typename B2, typename A1, typename A2>
+bind_t<R, member_fn<T,R,R (T::*)(B1,B2)>, arg_list3<C,A1,A2> >
+bind(R (T::*f)(B1,B2), C c, A1 a1, A2 a2) {
+    typedef R (T::*FN)(B1, B2);
+    typedef member_fn<T, R, FN> F;
+    arg_list3<C,A1,A2> l3(c, a1, a2);
+    return bind_t<R,F,arg_list3<C,A1,A2> >(F(f), l3);
 }
 
-// member function with 2 arguments, pass class object by reference, constant
-template <typename R, typename C, typename B1, typename B2, typename A1, typename A2>
-bind_t<R,member_fn<C,R,R (C::*)(B1,B2) const>,arg_list3<const C&,A1,A2> >
-bind(R (C::*f)(B1,B2) const, const C& c, A1 a1, A2 a2) {
-    typedef R (C::*FN)(B1, B2) const;
-    typedef member_fn<C, R, FN> F;
-    arg_list3<const C&,A1,A2> l3(c, a1, a2);
-    return bind_t<R,F,arg_list3<const C&,A1,A2> >(F(f), l3);
-}
-
-// member function with 2 arguments, pass class object by pointer
-template <typename R, typename C, typename B1, typename B2, typename A1, typename A2>
-bind_t<R,member_fn<C,R,R (C::*)(B1,B2)>,arg_list3<C*,A1,A2> >
-bind(R (C::*f)(B1,B2), C* c, A1 a1, A2 a2) {
-    typedef R (C::*FN)(B1, B2);
-    typedef member_fn<C, R, FN> F;
-    arg_list3<C*,A1,A2> l3(c, a1, a2);
-    return bind_t<R,F,arg_list3<C*,A1,A2> >(F(f), l3);
-}
-
-// member function with 2 arguments, pass class object by pointer, constant
-template <typename R, typename C, typename B1, typename B2, typename A1, typename A2>
-bind_t<R,member_fn<C,R,R (C::*)(B1,B2) const>,arg_list3<const C*,A1,A2> >
-bind(R (C::*f)(B1,B2) const, const C* c, A1 a1, A2 a2) {
-    typedef R (C::*FN)(B1, B2) const;
-    typedef member_fn<C, R, FN> F;
-    arg_list3<const C*,A1,A2> l3(c, a1, a2);
-    return bind_t<R,F,arg_list3<const C*,A1,A2> >(F(f), l3);
+// member function with 2 arguments, constant
+template <typename R, typename T, typename C, typename B1, typename B2, typename A1, typename A2>
+bind_t<R, member_fn<T,R,R (T::*)(B1,B2) const>, arg_list3<C,A1,A2> >
+bind(R (T::*f)(B1,B2) const, C c, A1 a1, A2 a2) {
+    typedef R (T::*FN)(B1, B2) const;
+    typedef member_fn<T, R, FN> F;
+    arg_list3<C,A1,A2> l3(c, a1, a2);
+    return bind_t<R,F,arg_list3<C,A1,A2> >(F(f), l3);
 }
 
 #endif
